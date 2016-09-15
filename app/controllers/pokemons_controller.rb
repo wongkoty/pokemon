71f2215408043eff2c9107203ffb887ba45057e6
Koty Wong <kotywong@Kotys-MBP.fios-router.home>
2016-09-15 00:14:18 -0400

    show page

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,7 +2,15 @@ class PokemonsController < ApplicationController
 	def index
 		response = HTTParty.get('http://pokeapi.co/api/v2/pokemon')
 		response = JSON.parse(response.body)
-		# pp response["results"]
+		pp response
 		@pokemons = response["results"]
 	end
+
+	def show
+		# puts params[:id]
+		response = HTTParty.get('http://pokeapi.co/api/v2/pokemon/' + params[:id])
+		pp JSON.parse(response.body)
+		@pokemon = JSON.parse(response.body)
+	end
+
 end
